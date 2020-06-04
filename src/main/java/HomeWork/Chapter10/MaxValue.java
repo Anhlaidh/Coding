8258b90808b179678c451be693a0cdc81c3bc461
Anhlaidh <chinahantianzhao@foxmail.com>
2020-06-04 15:15:26 +0800

    add maxvalue

--- src/main/java/HomeWork/Chapter10/MaxValue.java
+++ src/main/java/HomeWork/Chapter10/MaxValue.java
@@ -10,12 +10,23 @@ import java.util.Random;
 public class MaxValue {
     public static void main(String[] args) {
         Random random = new Random();
-//        int[] arr = random.ints(10, -100, 100);
-//        System.out.println(solution(arr));
+        int[] arr = random.ints(10, -100, 100).toArray();
+        arr = new int[]{-2, 11, -4, 13, -5, -2};
+        System.out.println(solution(arr));
 
     }
-//
-//    public static int solution(int[] arr) {
-//
-//    }
+
+    public static int solution(int[] arr) {
+        int[] dp = new int[arr.length];
+        for (int i = 0; i < dp.length; i++) {
+            if (i == 0) {
+                dp[i] = arr[i];
+                continue;
+            }
+            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+
+        }
+        return dp[dp.length - 1];
+
+    }
 }
