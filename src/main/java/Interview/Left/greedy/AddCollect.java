c81ea2bb56c5483a6e4da9a38b4fdc6bea6b3981
Anhlaidh <chinahantianzhao@foxmail.com>
2020-09-18 18:50:04 +0800

    add

---
+++ src/main/java/Interview/Left/greedy/AddCollect.java
@@ -0,0 +1,17 @@
+package Interview.Left.greedy;
+
+/**
+ * @Description: 用户可以在任何时候调用如下两个方法:
+ * - boolean isSameSet(V x,V y)查询到样本x和样本y是否属于同一个集合
+ * void union(V x, V y)把x和y各自所在的样本合并成一个集合
+ * - isSameSet和union方法代价越低越好
+ * 调用O(N)
+ * 调用频繁 O(1)
+ * @author: Anhlaidh
+ * @date: 2020-09-17 09:21
+ */
+public class AddCollect {
+    public static void main(String[] args) {
+
+    }
+}
